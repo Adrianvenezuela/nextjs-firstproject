f27a537a7615288fc2fb80a43f3387cff6984cdd
adrian Estrada <aestradacolina@gmail.com>
2023-05-31 01:50:31 -0400

    primer commit

---
+++ app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur neque
+        aperiam facilis dolorum quasi, nemo, consectetur voluptatem corrupti
+        quis unde delectus ducimus. Modi, repellat nesciunt! Cumque adipisci
+        tenetur ex aperiam. Eum quasi omnis neque, tenetur id iusto praesentium
+        quia, quam molestiae ipsam laboriosam! Quis, praesentium quisquam
+        maiores repudiandae ratione consectetur libero culpa consequuntur
+        voluptatem, commodi tempora quibusdam ad vero quod aliquid dolore. Omnis
+        dolor voluptatem nemo totam, doloribus a aliquid placeat quis quos
+        dignissimos, magnam mollitia fuga? Minima vitae fugiat nihil, autem,
+        quidem adipisci sequi molestias eligendi rerum sapiente perferendis
+        aperiam ab totam! Fugiat, soluta. Quod repellendus recusandae enim
+        culpa.
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,17 @@
+function servicesPage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio at
+        tenetur excepturi sed perspiciatis. Voluptatibus non maiores
+        repellendus, officia natus numquam ex, nulla saepe possimus, voluptatem
+        itaque atque asperiores. Obcaecati ipsa omnis debitis incidunt adipisci,
+        deleniti beatae aliquam, voluptatem ad consectetur doloremque? Voluptate
+        magni eum, quis velit omnis dignissimos sint!
+      </p>
+    </div>
+  );
+}
+
+export default servicesPage;
